f69bee6c4859068a743189dbdc149b9692e5e81c
Frank <52gafr1bwi@hft-stuttgart.de>
2018-03-22 22:35:03 +0100

    Add button and text

--- app/src/main/java/com/example/frank/test/MainActivity.java
+++ app/src/main/java/com/example/frank/test/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.frank.test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.btOk);
+        final TextView text = (TextView) findViewById(R.id.tvText);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText("abc");
+            }
+        });
+
     }
 }
